555c7482061894ab3a84a892b0365b11312c18fb
Shalaby <cr7.shalaby@gmail.com>
2022-11-11 19:33:43 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,10 @@ setup(
     license='MIT',
     author='abdo shalaby',
     author_email='abdoshalaby.dev@gmail.com',
+    keywords=['gui', 'audio'],
+    install_requires=['numpy==1.23.4', 'scipy==1.9.3', 'matplotlib==3.6.2', 'pyttsx3==2.90', 'ttkbootstrap==1.9.0',
+                      'Pillow==9.3.0', 'pydub==0.25.1'],
+    python_requires='>=3.10',
+
     description='Simple GUI Application To Manipulate Audio Files By Modifying The Audio Wave Form And Display It Using Python'
 )
