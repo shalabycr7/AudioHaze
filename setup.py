34da1035b79be24b94bcba78bbd51cb9e752889b
Shalaby <cr7.shalaby@gmail.com>
2022-11-11 19:17:44 +0200

    Updated requirements.txt And setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Audio-Signal-Processing-App-GUI-in-Python',
+    version='0.1.1',
+    packages=['AudioLib'],
+    url='https://github.com/shalabycr7/Audio-Signal-Processing-App-GUI-in-Python',
+    license='MIT',
+    author='abdo shalaby',
+    author_email='abdoshalaby.dev@gmail.com',
+    description='Simple GUI Application To Manipulate Audio Files By Modifying The Audio Wave Form And Display It Using Python'
+)
