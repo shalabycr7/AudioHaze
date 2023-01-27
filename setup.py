7a58eb464a851da1210630ae8c6a2c68421444ba
Abdulrahman Shalaby <abdoshalaby.dev@gmail.com>
2023-01-27 17:15:52 +0200

    Sync To Latest Packages Versions + Updated setup.py & requirements.txt

--- setup.py
+++ setup.py
@@ -6,12 +6,13 @@ setup(
     packages=['AudioLib'],
     url='https://github.com/shalabycr7/Audio-Signal-Processing-App-GUI-in-Python',
     license='MIT',
-    author='abdo shalaby',
+    author='Abdulrahman Shalaby',
     author_email='abdoshalaby.dev@gmail.com',
     keywords=['gui', 'audio'],
     install_requires=['numpy==1.23.4', 'scipy==1.9.3', 'matplotlib==3.6.2', 'pyttsx3==2.90', 'ttkbootstrap==1.9.0',
                       'Pillow==9.3.0', 'pydub==0.25.1'],
     python_requires='>=3.10',
 
-    description='Simple GUI Application To Manipulate Audio Files By Modifying The Audio Wave Form And Display It Using Python'
+    description='Simple GUI Application To Manipulate Audio Files By Modifying The Audio Wave Form And Display It '
+                'Using Python'
 )
