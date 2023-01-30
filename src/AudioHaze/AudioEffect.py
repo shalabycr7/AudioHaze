f4855d58a93f2625ad139c62f369c75a6a4ed74c
shalabycr7 <abdoshalaby.dev@gmail.com>
2023-01-30 14:54:11 +0200

    Fix Importing File issue In AudioEffect.py

--- src/AudioHaze/AudioEffect.py
+++ src/AudioHaze/AudioEffect.py
@@ -3,6 +3,6 @@ from . import AudioProcessing
 
 def echo(input_path, output_path):
     # Applies an echo effect to a given input audio file
-    sound = AudioProcessing(input_path)
+    sound = AudioProcessing.AudioProcessing(input_path)
     sound.set_echo(0.09)
     sound.save_to_file(output_path)
